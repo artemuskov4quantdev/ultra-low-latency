c1c9f1a4910a6919e70016c643a640af8aea6efe
Artem Uskov <a.uskov@bergmann-infotech.de>
2026-01-15 10:57:54 +0300

    Add project files.

---
+++ Engine.Core/Class1.cs
@@ -0,0 +1,16 @@
+﻿namespace Engine.Core;
+
+public readonly struct Event
+{
+    public readonly long Sequence;
+    public readonly int Type;
+    public readonly long Timestamp;
+
+    public Event(long sequence, int type, long timestamp)
+    {
+        Sequence = sequence;
+        Type = type;
+        Timestamp = timestamp;
+    }
+}
+
